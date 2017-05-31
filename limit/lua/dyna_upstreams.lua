233035de6262577dcbd47684592bf2ffda8e5997
zhangzhiyuan <zhiyuan.zhang@duobei.com>
2017-05-31 19:57:29 +0800

    更改openresty到macbook--->linux

--- limit/lua/dyna_upstreams.lua
+++ limit/lua/dyna_upstreams.lua
@@ -1,19 +1,17 @@
-
-local cjson = require "cjson"
-local http = require("socket.http")
 local ltn12 = require("ltn12")
+local http = require("socket.http")
+local cjson = require "cjson"
 
-dyna_upstreams = {}
 
-
---curl -X PUT http://127.0.0.1:8500/v1/catalog/register -d '{ "Datacenter": "dc1", "Node": "tomcat", "Address": "127.0.0.1", "Service": {"Id": "127.0.0.1:8080", "Service": "item_work_tomcat", "tags":["dev"], "Port": 8080} }'
-
-function dyna_upstreams.update_upstreams()
+--curl -X PUT http://10.211.55.14:8500/v1/catalog/register -d '{ "Datacenter": "dc1", "Node": "tomcat", "Address": "10.211.55.14", "Service": {"Id": "10.211.55.14", "Service": "item_work_tomcat", "tags":["dev"], "Port": 8080} }'
+--
+-- lua 模块还需要好好看下 local 问题好大
+local function update_upstreams()
     local resp = {}
     http.request{
         url =
-		    "http://127.0.0.1:8500/v1/catalog/service/item_work_tomcat",
-	        sink = ltn12.sink.table(resp)
+        "http://10.211.55.14:8500/v1/catalog/service/item_work_tomcat",
+        sink = ltn12.sink.table(resp)
     }
     resp = table.contact(resp)
     resp = cjson.decode(resp)
@@ -26,16 +24,12 @@ function dyna_upstreams.update_upstreams()
     ngx.shared.upstream_list:set("item_work_tomcat", cjson.encode(upstreams))
 end
 
-function dyna_upstreams.get_upstreams()
+local function get_upstreams()
     local upstreams_str = ngx.shared.upstream_list:get("item_work_tomcat")
 end
 
 
---local _M = {
---    update_upstreams = update_upstreams,
---    get_upstreams = get_upstreams
---}
-
-return dyna_upstreams
-
-   
+local _M = {
+    update_upstreams = update_upstreams,
+    get_upstreams = get_upstreams
+}
